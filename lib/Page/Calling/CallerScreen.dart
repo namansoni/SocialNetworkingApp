e6f88a3408d5027274029bcbd82e630fe872810f
pulkit807 <56972008+pulkit807@users.noreply.github.com>
2020-07-15 18:36:32 +0530

    Bug fixed

--- lib/Page/Calling/CallerScreen.dart
+++ lib/Page/Calling/CallerScreen.dart
@@ -55,29 +55,6 @@ class _CallerPageState extends State<CallerPage> {
 
   @override
   Widget build(BuildContext context) {
-    SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
-      callStreamSubscription=callMethods.callStream(uid: widget.currentUser.id).listen((DocumentSnapshot ds) {
-        switch (ds.data){
-          case null:
-            Navigator.pop(context);
-            break;
-          default:
-            return Scaffold(
-              backgroundColor: Colors.black,
-              body: Center(
-                child: Stack(
-                  children: <Widget>[
-                    _viewRows(),
-                    _panel(),
-                    _toolbar(context),
-                  ],
-                ),
-              ),
-            );
-            break;
-        }
-      });
-    });
     return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
